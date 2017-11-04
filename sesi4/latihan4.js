7282695fcd751f312a2a337297676e647131f7a0
Darmawan Sugiharto <dsdarma@gmail.com>
2017-11-04 11:58:32 +0700

    latihan

---
+++ sesi4/latihan4.js
@@ -0,0 +1,14 @@
+var jam = 19;
+if(jam >= 1 && jam <= 11){
+    console.log("Selamat Pagi");
+}
+else if (jam >=11 && jam <= 15){
+        console.log("Selamat Siang");
+    }
+    else if (jam >=15 && jam <=18){
+        console.log("Selamat Sore");
+        }
+        else {
+            console.log("Selamat Malam");
+        }
+        
