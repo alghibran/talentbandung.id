0f8a770894085393b59ff7e9ceac1ea7f55d6665
Darmawan Sugiharto <dsdarma@gmail.com>
2017-11-04 11:07:08 +0700

    update

---
+++ sesi4/latihan.js
@@ -0,0 +1,5 @@
+alert("hellooo");
+var nama = "darma";
+var webBasic = ["HTML", "CSS", "Javascript"];
+
+console.log (webBasic[0])
---
+++ sesi4/latihan2.js
@@ -0,0 +1,21 @@
+var buah = ["Apel", "Anggur", "Durian", "Pisang", "Nanas", "Melon", "Jeruk"]
+
+console.log(buah [0]);
+console.log(buah [4]);
+buah[4] = "Semangka";
+console.log(buah[4]);
+
+var person = {
+    name : "Darmawan S",
+    age : 18,
+    address : 'Padasuka Bandung',
+    gadget : {
+        HP : 'Android',
+        Laptop : 'ASUS'
+    },
+    hobby : ['Bola', 'Run']
+};
+
+console.log (person.name)
+console.log (person.gadget.laptop)
+console.log (person.hobby[1])
