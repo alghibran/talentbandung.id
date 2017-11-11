b2582c0892e2738534fd3bd9caa76d32e956c455
Darmawan Sugiharto <dsdarma@gmail.com>
2017-11-11 09:11:12 +0700

    update lagi

--- sesi5/tugas4.js
+++ sesi5/tugas4.js
@@ -4,13 +4,24 @@
 //     console.log(club[i]);
 // }
 
-var btnGreet = document.getElementsByClassName("greet");
-btnGreet.addEventListener("click",greet);
+// var btnGreet = document.getElementsByClassName("greet");
+// btnGreet.addEventListener("click",greet);
 
-function greet (event){
-    var greet = ["MU", "City", "Chelsea", "Arsenal", "Tottenham", "Everton", "Liverpool", "WHU", "Leichester", "Sunderland" ];
-    for(i=0; i<greet.length; i++){
-        alert(event.target.textContent);
-    }
-    // console.log(event.target.textContent);
-}
+// function greet (event){
+//     var greet = ["MU", "City", "Chelsea", "Arsenal", "Tottenham", "Everton", "Liverpool", "WHU", "Leichester", "Sunderland" ];
+//     for(i=0; i<greet.length; i++){
+
+//         alert(event.target.textContent);
+//     }
+//     // console.log(event.target.textContent);
+// }
+
+function clubing (event){
+    var nama = event.target.textContent;
+    return alert('halo' + nama);
+  }
+  
+  var club = document.getElementsByClassName(' club');
+  for (var i = 0; i < club.length; i++) {
+    club[i].addEventListener('click', clubing );
+  }
